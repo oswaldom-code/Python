a384c8d65dd274ac40bfbbedd16b5d3276345482
Oswaldo Montano <oswaldoeliezer876@gmail.com>
2017-06-27 08:29:29 -0400

    Carga de algunos ejercicios básicos desde la oficina

---
+++ basics/number_random.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import random
+import os
+
+
+
+
+
+
+
+def run():
+    number_found = False
+    os.system('cls') #Clear the console
+    print('=====================================')
+    print('..:: EN BUSCA DEL NÚMERO SECRETO ::..')
+    print('=====================================')
+    aleatorio = calc_random()
+   
+    while not number_found:
+        number = int(input('\aAdivida el numero secreto: '))
+
+        if number == aleatorio:
+            os.system('cls') #Clear the console
+            print('=================================')
+            print('****         E X I T O       **** ')
+            print('****  Encontraste el Número! ****')
+            print('=================================')
+            
+            number_found = True
+            pass
+        elif number > aleatorio:
+            print('\aMejor intenta con un número más bajo...')
+               
+        else:
+            print('\aMejor intenta con un número más alto..')       
+        
+
+
+#funtion of  aleatorio number claculation
+def calc_random ():
+    
+    print('>>> Primero definamos el rango <<<')
+    numberInit = int(input('>Desde el número: '))
+    numberEnd = int(input('>Hasta el número: '))
+    os.system('cls') #Clear the console
+    print('======================')
+    print(' ****   A JUGAR   **** ')
+    print('======================')
+    aleatorio = random.randint(numberInit,numberEnd)
+    return aleatorio
+ 
+if __name__ == '__main__':
+    run()
+
+
+   
---
+++ basics/numeros_primos.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+def is_prime(number):
+    for  1 in number:
+        print('Iteració {}'.format())
+
+
+
+def run():
+    #print('Escribe un número: ')
+    number = int(input('Escribe un número: '))
+    is_prime(number)
+
+
+
+if __name__ == '__main__':
+    run()
