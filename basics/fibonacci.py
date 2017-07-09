1a7bc74d829ccc34e33dee380bd3c51fc3b547f2
@oswaldom876 (HOME) <oswaldoeliezer876@gmail.com>
2017-07-09 11:46:44 -0400

    Comentarios al codigo

--- basics/fibonacci.py
+++ basics/fibonacci.py
@@ -1,11 +1,14 @@
 
-    def fib(n):
-        """Escribe la serie de Fibonacci hasta n."""
-        a, b = 0, 1
-        while a < n:
-            print(a, " ")
-            a, b = b, a+b
-            print()
+# Run with python3
+#Escribe la serie de Fibonacci hasta n
+
+def fib(n):
+    
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
 
 if __name__ == '__main__':
     fib(100)
